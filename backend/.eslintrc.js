ad0e24fc378af3f0d415108759351dafc3cf3c89
Alexey Soloviev <alexey.soloviev.w@gmail.com>
2021-06-06 21:20:40 +0300

    Backend. Bootstrap a basic NestJS server

---
+++ backend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "airbnb-base",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: [
+        "@typescript-eslint",
+    ],
+    rules: {
+        "class-methods-use-this": 0,
+        "import/prefer-default-export": 0,
+        indent: ["error", 4, { SwitchCase: 1 }],
+        quotes: [2, "double"],
+        "max-len": ["error", { code: 100 }],
+        "no-empty-function": 0,
+        "no-unused-vars": ["error", { args: "none" }],
+        "no-useless-constructor": 0,
+    },
+};
